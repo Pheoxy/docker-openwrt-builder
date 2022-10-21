b2adf78c45a7d2486346a038a48f840c1d010acf
Daniel Hancock <pheoxy@gmail.com>
2022-10-21 16:33:05 +0800

    Fix cannot stat

--- build/build.sh
+++ build/build.sh
@@ -1,11 +1,10 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 SOURCE_PATH="/home/openwrt-builder/source"
 OUTPUT_PATH="/home/openwrt-builder/output"
 
-CORES="$(nproc)"
-DATETIME="$(date +"%Y%m%d_%H%M")"
-TARGET_FOLDER="$BUILD_TARGET-$DATETIME"
+# Move to source directory.
+cd $SOURCE_PATH
 
 prepare() {
     if [ "$OLD_CONFIG" = y ]; then
@@ -30,14 +29,14 @@ prepare() {
 build() {
     # Make output folders
     echo -e "$LIGHT_CYAN--> Starting compile... $NC"
-    mkdir -p "$OUTPUT_PATH"/"$TARGET_FOLDER"
-    ./scripts/diffconfig.sh >"$OUTPUT_PATH"/"$TARGET_FOLDER"/config.builddiff
+    mkdir -p "$OUTPUT_PATH"/"$BUILD_TARGET"
+    ./scripts/diffconfig.sh >"$OUTPUT_PATH"/"$BUILD_TARGET"/config.builddiff
 
     # Check for debug and compile
     if [ "$DEBUG" = y ]; then
         echo -e "running with debug enabled"
         # time ionice -c 3 nice -n 20 make -j1 V=s 2>&1 | tee $OUTPUT_PATH/make.log | grep -i error
-        time ionice -c 3 nice -n 20 make -j1 V=s 2>&1 | tee "$OUTPUT_PATH"/"$TARGET_FOLDER"/make.log
+        time ionice -c 3 nice -n 20 make -j1 V=s 2>&1 | tee "$OUTPUT_PATH"/"$BUILD_TARGET"/make.log
     else
         make download
         time ionice -c 3 nice -n 19 make -j"$CORES"
@@ -46,8 +45,8 @@ build() {
 
 package() {
     # Copying to output folder
-    echo -e "$LIGHT_CYAN--> Copying files to output folder $OUTPUT_PATH/$TARGET_FOLDER $NC"
-    cp -r "$SOURCE_PATH"/bin/targets/* "$OUTPUT_PATH"/"$TARGET_FOLDER"/
+    echo -e "$LIGHT_CYAN--> Copying files to output folder $OUTPUT_PATH/$BUILD_TARGET $NC"
+    cp -r "$SOURCE_PATH"/bin/targets/* "$OUTPUT_PATH"/"$BUILD_TARGET"/
     # sed -i '/^#/d' $SOURCE_PATH/output/$(date +%Y%m%d%H%M)/generic/config.seed
 }
 
